55038171d0b2aaccc85b1f5b215367aed530aa4d
Aleoru <57480716+Aleoru@users.noreply.github.com>
2022-05-05 16:20:06 +0200

    Add files via upload

---
+++ C/ring_0/libft/ft_lstclear.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoropeza <aoropeza@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 14:40:05 by aoropeza          #+#    #+#             */
+/*   Updated: 2022/05/05 15:06:17 by aoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list	**lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst)
+		return ;
+	while (*lst != NULL)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+	free(temp);
+}
