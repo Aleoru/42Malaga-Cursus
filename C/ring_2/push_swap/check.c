a6b0bec61f9c7bb5c01410c202a30a4b90dc9775
Aleoru <aleoru0022@gmail.com>
2022-11-10 18:23:13 +0000

    push_swap

--- C/ring_2/push_swap/check.c
+++ C/ring_2/push_swap/check.c
@@ -11,3 +11,27 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void    check_arg(char **argv, t_data *data)
+{
+	int		i;
+	int		j;
+	char	**array;
+
+	i = 0;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (!ft_isalpha(argv[i][j]) && argv[i][j] != '\0')
+			{
+				array = ft_split (argv[i], argv[i][j]);
+				break ;
+			}
+			j++;
+		}
+		if (argv[i][j] == '\0')
+			i++;
+	}
+}
