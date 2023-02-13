dfb9df8d33941bd980db410d40c35bbfba15e64e
Alejandra Oropeza <57480716+Aleoru@users.noreply.github.com>
2023-02-13 20:25:26 +0100

    Add files via upload

---
+++ C/ring_3/minishell/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoropeza <aoropeza@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 18:52:27 by aoropeza          #+#    #+#             */
+/*   Updated: 2023/02/13 18:52:28 by aoropeza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+
+/* Necesarias para usar readline */
+# include <readline/readline.h>
+# include <readline/history.h>
+
+# include "./libft/inc/libft.h"
+
+#endif
