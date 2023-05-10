59b49bdc532e492a526cfcd996a9902cb903c224
Alejandra Oropeza <57480716+Aleoru@users.noreply.github.com>
2023-05-10 15:31:26 +0200

    Add files via upload

--- C/ring_4/CPP Module 00/ex02/Account.cpp
+++ C/ring_4/CPP Module 00/ex02/Account.cpp
@@ -22,6 +22,7 @@ Account::Account(int initial_deposit){
 
 Account::~Account(void){
 
+	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount;
 	std::cout << ";closed" << std::endl;
 
@@ -60,11 +61,14 @@ void	Account::makeDeposit(int deposit){
 
 	int	p_amount = this->_amount;
 
-	this->_amount = p_amount + deposit;
+	this->_amount += deposit;
 	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";p_amount:" << p_amount;
-	std::cout << ";deposit:" << deposit << ";nb_deposits:" << this->_nbDeposits << std::endl;
+	std::cout << ";deposit:" << deposit << ";amount:" << this->_amount;
+	std::cout << ";nb_deposits:" << this->_nbDeposits << std::endl;
+	Account::_totalAmount += deposit;
 
 }
 
@@ -72,12 +76,15 @@ bool	Account::makeWithdrawal(int withdrawal){
 
 	int	p_amount = this->_amount;
 
-	if (withdrawal > p_amount) {
-		this->_amount = p_amount - withdrawal;
+	if (withdrawal <= p_amount) {
+		this->_amount -= withdrawal;
 		this->_nbWithdrawals++;
+		Account::_totalNbWithdrawals++;
 		Account::_displayTimestamp();
 		std::cout << "index:" << this->_accountIndex << ";p_amount:" << p_amount;
-		std::cout << ";withdrawal:" << withdrawal << ";nb_withdrawal:" << this->_nbWithdrawals << std::endl;
+		std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->_amount;
+		std::cout << ";nb_withdrawal:" << this->_nbWithdrawals << std::endl;
+		Account::_totalAmount -= withdrawal;
 		return (true);
 	}
 	else {
